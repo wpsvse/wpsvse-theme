df1b01cf06726966b2c2e0fae9a64490414c98ea
Mattias Tengblad <mst@eyesx.com>
2014-08-21 13:07:16 +0200

    Flytta sidans titel för forum

--- forum.php
+++ forum.php
@@ -14,7 +14,7 @@ get_header();
 		<div class="container">
 			<div class="row">
               <div class="col-md-12">
-            	<h1 class="page-title"><?php the_title(); ?></h1>
+            	<h1 class="page-title">Forum</h1>
               </div>
             </div>
         </div>
@@ -26,6 +26,7 @@ get_header();
 		<div class="container">
 			<div class="row">
               <div class="col-md-12">
+                <h2><?php the_title(); ?></h2>
               	<?php the_content(); ?>
               </div>
 			</div>
