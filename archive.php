78d4395a842c260640e7258340d1c2356b9e088c
Mattias Tengblad <mst@eyesx.com>
2014-08-25 20:15:28 +0200

    Uppdateringar för inlägg

--- archive.php
+++ archive.php
@@ -9,13 +9,14 @@
 
 get_header(); ?>
 
-	<section id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
 		<?php if ( have_posts() ) : ?>
 
-			<header class="page-header">
-				<h1 class="page-title">
+	<!-- Start Page Header -->
+    <section id="page-header" class="section">
+		<div class="container">
+			<div class="row">
+              <div class="col-md-12">
+            	<h1 class="page-title">
 					<?php
 						if ( is_category() ) :
 							single_cat_title();
@@ -44,59 +45,50 @@ get_header(); ?>
 						elseif ( is_year() ) :
 							printf( __( 'Year: %s', 'wpsvse' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
 
-						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
-							_e( 'Asides', 'wpsvse' );
-
-						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-							_e( 'Images', 'wpsvse');
-
-						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
-							_e( 'Videos', 'wpsvse' );
-
-						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
-							_e( 'Quotes', 'wpsvse' );
-
-						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
-							_e( 'Links', 'wpsvse' );
-
 						else :
 							_e( 'Archives', 'wpsvse' );
 
 						endif;
 					?>
 				</h1>
-				<?php
-					// Show an optional term description.
-					$term_description = term_description();
-					if ( ! empty( $term_description ) ) :
-						printf( '<div class="taxonomy-description">%s</div>', $term_description );
-					endif;
-				?>
-			</header><!-- .page-header -->
+              </div>
+            </div>
+        </div>
+    </section>
+    <!-- Start Page Header -->
 
-			<?php /* Start the Loop */ ?>
-			<?php while ( have_posts() ) : the_post(); ?>
+    <!-- Start Page Content -->
+	<section id="page-full" class="section">
+		<div class="container">
+			<div class="row">
+              <div class="col-md-9">
 
-				<?php
-					/* Include the Post-Format-specific template for the content.
-					 * If you want to override this in a child theme, then include a file
-					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-					 */
-					get_template_part( 'content', get_post_format() );
-				?>
+				<?php /* Start the Loop */ ?>
+                <?php while ( have_posts() ) : the_post(); ?>
+    
+                    <?php
+                        /* Include the Post-Format-specific template for the content.
+                         * If you want to override this in a child theme, then include a file
+                         * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                         */
+                        get_template_part( 'content' );
+                    ?>
+    
+                <?php endwhile; ?>
+    
+                <?php wpsvse_content_nav( 'nav-below' ); ?>
+    
+            <?php else : ?>
+    
+                <?php get_template_part( 'no-results', 'archive' ); ?>
+    
+            <?php endif; ?>
 
-			<?php endwhile; ?>
+              </div>
+              <?php get_sidebar(); ?>
+			</div>
+		</div>
+	</section>
+	<!-- End Page Content -->
 
-			<?php wpsvse_content_nav( 'nav-below' ); ?>
-
-		<?php else : ?>
-
-			<?php get_template_part( 'no-results', 'archive' ); ?>
-
-		<?php endif; ?>
-
-		</main><!-- #main -->
-	</section><!-- #primary -->
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
