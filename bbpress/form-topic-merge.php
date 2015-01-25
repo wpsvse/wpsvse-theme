eb55a08633d92697fb86abf4867c2b00689dbf39
Mattias Tengblad <mst@eyesx.com>
2015-01-25 16:18:59 +0100

    Uppdaterad formulärinramning för forum

--- bbpress/form-topic-merge.php
+++ bbpress/form-topic-merge.php
@@ -17,9 +17,15 @@
 
 			<form id="merge_topic" name="merge_topic" method="post" action="<?php the_permalink(); ?>">
 
-				<fieldset class="bbp-form">
-
-					<legend><?php printf( __( 'Merge topic "%s"', 'bbpress' ), bbp_get_topic_title() ); ?></legend>
+			<div class="panel panel-primary">
+				<div class="panel-heading"><h3 class="panel-title" data-toggle="collapse" data-target="#bbp-reply-panel" aria-expanded="false" aria-controls="bbp-reply-panel"><i class="fa fa-plus-square"></i>
+                	
+					<?php printf( __( 'Merge topic "%s"', 'bbpress' ), bbp_get_topic_title() ); ?>
+                    
+				</h3>
+            </div>
+            
+            <div class="panel-body collapse in" id="bbp-reply-panel">
 
 					<div>
 
@@ -95,7 +101,9 @@
 
 					<?php bbp_merge_topic_form_fields(); ?>
 
-				</fieldset>
+			</div>
+            
+            </div>
 			</form>
 		</div>
 
