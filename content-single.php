ad3bd7990ea87e570b5cd059901fb5aaedef41d9
Mattias Tengblad <mst@eyesx.com>
2015-02-05 01:34:24 +0100

    Uppdatering för bloggmallar
    Åtgärdar bl.a wpsvse/wpsv.se#56 och förbättrad arkivvisning

--- content-single.php
+++ content-single.php
@@ -20,11 +20,11 @@
     
     <div class="single-content clearfix">
         <h1 class="single-entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
-        <div class="single-meta-header"><span><i class="fa fa-clock-o"></i> <?php the_time('j F'); ?></span> <span><i class="fa fa-folder"></i> <?php the_category(', '); ?></span> <?php the_tags( '<span><i class="fa fa-tags"></i> ', ', ', '</span>' ); ?><?php edit_post_link('Redigera', '<span><i class="fa fa-pencil"></i> ', '</span>'); ?></div>
+        <div class="single-meta-header"><span><i class="fa fa-clock-o"></i> <?php the_time('j F'); ?></span> <span><i class="fa fa-folder"></i> <?php if ( 'wpsvse_blog' == get_post_type() ) { echo get_the_term_list( $post->ID, 'blog_category', '', ', ', '' ); } else { the_category(', '); } ?></span> <?php the_tags( '<span><i class="fa fa-tags"></i> ', ', ', '</span>' ); ?><?php edit_post_link('Redigera', '<span><i class="fa fa-pencil"></i> ', '</span>'); ?></div>
         <div class="single-content"><?php the_content(); ?></div>
         <div class="single-footer-meta">
             <button class="popover-link" data-html="true" data-container="body" data-placement="top" data-toggle="popover" data-trigger="focus" data-content="Det här inlägget publicerades av <strong><?php the_author(); ?></strong> för WordPress Sverige"><?php echo get_avatar( get_the_author_meta('ID'), 96 ); ?></button>
-            <button class="popover-link" data-html="true" data-container="body" data-placement="top" data-toggle="popover" data-trigger="focus" data-content="Direktlänk/kortlänk till denna nyhet <a href='<?php echo wp_get_shortlink(); ?>'><?php echo wp_get_shortlink(); ?></a>"><i class="fa fa-bookmark"></i></button>
+            <button class="popover-link" data-html="true" data-container="body" data-placement="top" data-toggle="popover" data-trigger="focus" data-content="Direktlänk/kortlänk till denna <?php if ( 'wpsvse_blog' == get_post_type() ) { ?>bloggpost<?php } else { ?>nyhet<?php } ?> <a href='<?php echo wp_get_shortlink(); ?>'><?php echo wp_get_shortlink(); ?></a>"><i class="fa fa-bookmark"></i></button>
         </div>
     </div><!-- .entry-meta -->
     
