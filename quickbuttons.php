6c51384e997c0894f753716ebebf1a0f72d9852a
marcus.tisater@happiness.se <marcus.tisater@happiness.se>
2015-05-13 19:29:24 +0200

    Added some grid fixes for quickbuttons and front-page stacking

--- quickbuttons.php
+++ quickbuttons.php
@@ -6,28 +6,28 @@
  */
 ?>
 
-            	<div id="support-btn" class="col-md-3 quick-btn">
+            	<div id="support-btn" class="col-sm-6 col-md-3 quick-btn">
                 	<a href="<?php echo esc_url( home_url( '/support/' ) ); ?>" class="quick-link">
                         <i class="fa fa-medkit"></i>
                         <h2>Support</h2>
                         <span>Ta del av support för WordPress</span>
                     </a>
                 </div>
-                <div id="forum-btn" class="col-md-3 quick-btn">
+                <div id="forum-btn" class="col-sm-6 col-md-3 quick-btn">
                 	<a href="<?php echo esc_url( home_url( '/forum/' ) ); ?>" class="quick-link">
                         <i class="fa fa-comments"></i>
                         <h2>Forum</h2>
                         <span>Diskussioner och hjälp via vårt forum</span>
                     </a>
                 </div>
-                <div id="files-btn" class="col-md-3 quick-btn">
+                <div id="files-btn" class="col-sm-6 col-md-3 quick-btn">
                 	<a href="<?php echo esc_url( home_url( '/filer/' ) ); ?>" class="quick-link">
                         <i class="fa fa-cloud-download"></i>
                         <h2>Filer</h2>
                         <span>Ladda ner översättningar, tillägg och teman</span>
                     </a>
                 </div>
-                <div id="community-btn" class="col-md-3 quick-btn">
+                <div id="community-btn" class="col-sm-6 col-md-3 quick-btn">
                 	<a href="<?php echo esc_url( home_url( '/community/' ) ); ?>" class="quick-link">
                         <i class="fa fa-group"></i>
                         <h2>Community</h2>
