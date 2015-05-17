c30140d580a4a3a161e4b386685c99bdbfba2049
Mattias Tengblad <mst@eyesx.com>
2015-05-17 22:15:47 +0200

    <link> for favicon

--- header.php
+++ header.php
@@ -15,6 +15,7 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<link rel="shortcut icon" type='image/x-icon' href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 
 <?php wp_head(); ?>
 </head>
