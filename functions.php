bedf68e6a3ed7f24218e92549afe8c00e2c18bab
Mattias Tengblad <mst@eyesx.com>
2015-06-30 05:09:04 +0200

    Add metabox class (CMB2)

--- functions.php
+++ functions.php
@@ -142,32 +142,21 @@ function wpsvse_scripts() {
 
 	// Enqueue Stylesheets
 	wp_enqueue_style( 'bootstrap-style', get_template_directory_uri() . '/css/bootstrap.min.css' );
-
 	wp_enqueue_style( 'fontawesome-style', get_template_directory_uri() . '/css/font-awesome.min.css' );
-
 	wp_enqueue_style( 'dropdowns-enhancement-style', get_template_directory_uri() . '/css/dropdowns-enhancement.min.css' );
-
 	// wp_enqueue_style( 'bbpress-style', get_template_directory_uri() . '/css/bbpress.css' );
-
 	wp_enqueue_style( 'wpsvse-style', get_stylesheet_uri() );
 
 	// Engueue Javascripts
-	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.2.0', true );
-
+	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/bootstrap.min.js', array('jquery'), '3.3.2', true );
 	wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/js/dropdowns-enhancement.js', array('jquery'), '3.2.0', true );
-
 	wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/js/modernizr.min.js', array(), '2.8.3' );
-
 	wp_enqueue_script( 'HTML5shiv', get_template_directory_uri() . '/js/html5shiv.js', array('jquery'), '3.7.0' );
-
 	wp_enqueue_script( 'respond_js', get_template_directory_uri() . '/js/respond.min.js', array('jquery'), '1.3.0' );
-
 	wp_enqueue_script( 'magnific_popup', get_template_directory_uri() . '/js/jquery.magnific-popup.min.js', array('jquery'), '1.0.0' );
-
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-
 	wp_enqueue_script( 'wpsvse-scripts', get_template_directory_uri() . '/js/wpsvse.js', array(), '20140719', true );
 
 }
@@ -187,3 +176,8 @@ require get_template_directory() . '/inc/extras.php';
  * Custom functions for the adminbar.
  */
 require get_template_directory() . '/inc/adminbar.php';
+
+/**
+ * Custom functions metaboxes
+ */
+require get_template_directory() . '/inc/metaboxes.php';
