b2d721091b9086214ece6cbf238e98e01ef833cc
Mattias Tengblad <mst@eyesx.com>
2015-07-03 13:22:26 +0200

    Styling fixes for front page downloads

--- download-monitor/content-download-front.php
+++ download-monitor/content-download-front.php
@@ -5,14 +5,10 @@
 
 global $dlm_download;
 ?>
-<a class="btn btn-dl" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow"><i class="fa fa-cloud-download"></i>
+<a class="btn btn-dl download-link-front" href="<?php $dlm_download->the_download_link(); ?>" rel="nofollow"><i class="fa fa-cloud-download"></i>
 	<?php printf( __( '%s ', 'download-monitor' ), $dlm_download->get_the_title() ); 
 	if ( $dlm_download->has_version_number() ) {
 		printf( __( '%s', 'download-monitor' ), $dlm_download->get_the_version_number() );
-	} ?>&nbsp;<span>
-	<?php
-	remove_filter('the_excerpt', 'wpautop');
-	the_excerpt();
-	?></span>
+	} ?>&nbsp;<?php	the_excerpt(); ?>
 	<br /><span>Det officiella paketet från wordpress.org</span>
 </a>
