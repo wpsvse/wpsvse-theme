5f939db76babb318163cd75f874d222297d6495e
Mattias Tengblad <mst@eyesx.com>
2015-07-08 16:12:00 +0200

    Some translation

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -35,8 +35,8 @@ function wpsvse_content_nav( $nav_id ) {
 
 	<?php if ( is_single() ) : // navigation links for single posts ?>
 
-		<?php previous_post_link( '<div class="nav-previous">%link</div>', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'wpsvse' ) . '</span> %title' ); ?>
-		<?php next_post_link( '<div class="nav-next">%link</div>', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'wpsvse' ) . '</span>' ); ?>
+		<?php previous_post_link( '<div class="nav-previous">%link</div>', '<span class="meta-nav">' . _x( '&larr;', 'Föregående inlägg', 'wpsvse' ) . '</span> %title' ); ?>
+		<?php next_post_link( '<div class="nav-next">%link</div>', '%title <span class="meta-nav">' . _x( '&rarr;', 'Nästa inlägg', 'wpsvse' ) . '</span>' ); ?>
 
 	<?php elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) : // navigation links for home, archive, and search pages ?>
 
@@ -44,7 +44,7 @@ function wpsvse_content_nav( $nav_id ) {
 			<?php if(function_exists('wp_pagenavi')) {
 				wp_pagenavi(); 
 			} else { ?>
-      <div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'wpsvse' ) ); ?></div>
+      <div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Äldre inlägg', 'wpsvse' ) ); ?></div>
       <?php }
 		endif; ?>
 
@@ -52,7 +52,7 @@ function wpsvse_content_nav( $nav_id ) {
 			<?php if(function_exists('wp_pagenavi')) {
 				wp_pagenavi(); 
 			} else { ?>
-      <div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'wpsvse' ) ); ?></div>
+      <div class="nav-next"><?php previous_posts_link( __( 'Nyare inlägg <span class="meta-nav">&rarr;</span>', 'wpsvse' ) ); ?></div>
       <?php }
 		endif; ?>
 
