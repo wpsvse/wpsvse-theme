ba302f6ab86d093b9532198f7f34416bcba2984a
Mattias Tengblad <mst@eyesx.com>
2015-07-10 00:32:33 +0200

    More legacy template updates

--- buddypress/activity/activity-loop.php
+++ buddypress/activity/activity-loop.php
@@ -1,14 +1,13 @@
-<?php do_action( 'bp_before_activity_loop' ); ?>
+<?php
+
+/**
+ * Fires before the start of the activity loop.
+ *
+ * @since BuddyPress (1.2.0)
+ */
+do_action( 'bp_before_activity_loop' ); ?>
 
 <?php if ( bp_has_activities( bp_ajax_querystring( 'activity' ) ) ) : ?>
-
-	<?php /* Show pagination if JS is not enabled, since the "Load More" link will do nothing */ ?>
-	<noscript>
-		<div class="pagination">
-			<div class="pag-count"><?php bp_activity_pagination_count(); ?></div>
-			<div class="pagination-links"><?php bp_activity_pagination_links(); ?></div>
-		</div>
-	</noscript>
 
 	<?php if ( empty( $_POST['page'] ) ) : ?>
 
@@ -25,7 +24,7 @@
 	<?php if ( bp_activity_has_more_items() ) : ?>
 
 		<li class="load-more">
-			<a href="#more"><?php _e( 'Load More', 'buddypress' ); ?></a>
+			<a href="<?php bp_activity_load_more_link() ?>"><?php _e( 'Load More', 'buddypress' ); ?></a>
 		</li>
 
 	<?php endif; ?>
@@ -44,10 +43,21 @@
 
 <?php endif; ?>
 
-<?php do_action( 'bp_after_activity_loop' ); ?>
+<?php
 
-<form action="" name="activity-loop-form" id="activity-loop-form" method="post">
+/**
+ * Fires after the finish of the activity loop.
+ *
+ * @since BuddyPress (1.2.0)
+ */
+do_action( 'bp_after_activity_loop' ); ?>
 
-	<?php wp_nonce_field( 'activity_filter', '_wpnonce_activity_filter' ); ?>
+<?php if ( empty( $_POST['page'] ) ) : ?>
 
-</form>
+	<form action="" name="activity-loop-form" id="activity-loop-form" method="post">
+
+		<?php wp_nonce_field( 'activity_filter', '_wpnonce_activity_filter' ); ?>
+
+	</form>
+
+<?php endif; ?>
