4e1390371f882e9b59811462c446a4682c86ba0b
Mattias Tengblad <mst@eyesx.com>
2015-07-18 08:11:20 +0200

    BuddyPress Docs templates for codex

--- community.php
+++ community.php
@@ -13,11 +13,11 @@ get_header();
     <section id="page-header" class="section">
 		<div class="container">
 			<div class="row">
-              <div class="col-md-12">
-            	<h1 class="page-title"><?php the_title(); ?></h1>
-              </div>
-            </div>
-        </div>
+				<div class="col-md-12">
+					<h1 class="page-title"><?php if ( 'bp_doc' == get_post_type() || is_post_type_archive('bp_doc') ) { echo 'Dokumentation'; } else { the_title(); } ?></h1>
+				</div>
+			</div>
+		</div>
     </section>
     <!-- Start Page Header -->
 
@@ -25,9 +25,16 @@ get_header();
 	<section id="page-full" class="section">
 		<div class="container">
 			<div class="row">
-              <div class="col-md-12">
-              	<?php the_content(); ?>
-              </div>
+			<?php if ( 'bp_doc' == get_post_type() || is_post_type_archive('bp_doc') ) { ?>
+				<div class="col-md-9 page-content">
+					<?php the_content(); ?>
+				</div>
+				<?php include ( bp_docs_locate_template( 'single/sidebar.php' ) ) ?>
+			<?php } else { ?>
+				<div class="col-md-12">
+					<?php the_content(); ?>
+				</div>
+			<?php } ?>
 			</div>
 		</div>
 	</section>
