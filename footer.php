a5ff5845aac0db432c83bdc86139439b349ab24e
Mattias Tengblad <mst@eyesx.com>
2015-09-28 07:19:38 +0200

    Do not use hard coded names

--- footer.php
+++ footer.php
@@ -42,8 +42,8 @@
 		<div class="container">
 			<div class="row">
 				<div class="section-headline nomargin-bottom white-heading">
-					<h2>Följ WordPress Sverige</h2>
-					<span>Få flöden serverade från WordPress Sverige och den svenska communityn</span>
+					<h2>Följ <?php bloginfo( 'name' ); ?></h2>
+					<span>Få flöden serverade från <?php bloginfo( 'name' ); ?> och den svenska communityn</span>
 				</div>
 			</div>
 			<div class="row">
@@ -51,10 +51,10 @@
 					<div class="social-container">
             <ul class="social">
              <!-- Start FOLLOW WORDPRESS -->
-              <li class="facebook"><a href="https://www.facebook.com/wpsv.se" class="wpsvse-tooltip" data-toggle="tooltip" data-placement="top" title="" data-original-title="WordPress Sveriges Facebooksida"><i class="fa fa-facebook"></i></a></li>
-              <li class="fb-group"><a href="https://www.facebook.com/groups/wpsvse/" class="wpsvse-tooltip" data-toggle="tooltip" data-placement="top" title="" data-original-title="WordPress Sveriges officiella Facebookgrupp"><i class="fa fa-facebook-square"></i></a></li>
-              <li class="twitter"><a href="https://twitter.com/WPSverige" class="wpsvse-tooltip" data-toggle="tooltip" data-placement="top" title="" data-original-title="WordPress Sverige på Twitter"><i class="fa fa-twitter"></i></a></li>
-              <li class="rss"><a href="<?php bloginfo('rss2_url'); ?>" class="wpsvse-tooltip" data-toggle="tooltip" data-placement="top" title="" data-original-title="WordPress Sveriges RSS-flöden"><i class="fa fa-rss"></i></a></li>
+              <li class="facebook"><a href="https://www.facebook.com/wpsv.se" class="wpsvse-tooltip" data-toggle="tooltip" data-placement="top" title="" data-original-title="<?php bloginfo( 'name' ); ?>s Facebooksida"><i class="fa fa-facebook"></i></a></li>
+              <li class="fb-group"><a href="https://www.facebook.com/groups/wpsvse/" class="wpsvse-tooltip" data-toggle="tooltip" data-placement="top" title="" data-original-title="<?php bloginfo( 'name' ); ?>s officiella Facebookgrupp"><i class="fa fa-facebook-square"></i></a></li>
+              <li class="twitter"><a href="https://twitter.com/WPSverige" class="wpsvse-tooltip" data-toggle="tooltip" data-placement="top" title="" data-original-title="<?php bloginfo( 'name' ); ?> på Twitter"><i class="fa fa-twitter"></i></a></li>
+              <li class="rss"><a href="<?php bloginfo('rss2_url'); ?>" class="wpsvse-tooltip" data-toggle="tooltip" data-placement="top" title="" data-original-title="<?php bloginfo( 'name' ); ?>s RSS-flöden"><i class="fa fa-rss"></i></a></li>
              <!-- End FOLLOW WORDPRESS -->
             </ul>
 					</div>
@@ -72,9 +72,9 @@
 					<p><i class="fa fa-info-circle"></i> <abbr title="Gnu Public Licens version 2">GPLv2</abbr> - 2007-<?php echo date('Y'); ?> <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
 				</div>
 				<div class="col-md-7">
-                  <!-- Start FOOTER MENU -->
-                    <?php wp_nav_menu( array( 'theme_location' => 'footer','container' => false,'items_wrap' => '<ul id="menu-footer" class="hidden-xs footer-links pull-right %2$s">%3$s</ul>' ) ); ?>
-                  <!-- End FOOTER MENU -->
+					<!-- Start FOOTER MENU -->
+						<?php wp_nav_menu( array( 'theme_location' => 'footer','container' => false,'items_wrap' => '<ul id="menu-footer" class="hidden-xs footer-links pull-right %2$s">%3$s</ul>' ) ); ?>
+					<!-- End FOOTER MENU -->
 				</div>
 			</div>
 		</div>
