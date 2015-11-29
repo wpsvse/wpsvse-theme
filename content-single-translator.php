3b147176f18250ead9ea59fae51880c4860e9a98
Mattias Tengblad <mst@eyesx.com>
2015-11-29 10:27:10 +0100

    Start for new validator template layout

--- content-single-translator.php
+++ content-single-translator.php
@@ -12,13 +12,13 @@
 					$user = get_user_by( 'slug', $translation_user );
 					$user_email = $user->user_email;
 				?>
-        <h2 class="project-entry-title"><?php echo get_avatar( $user_email, 128 ); ?> <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+        <div class="project-header"><h2 class="project-entry-title"><?php echo get_avatar( $user_email, 128 ); ?> <a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2></div>
         
 				<div class="project-meta">
 					<div class="project-sv-translate"><i class="fa fa-wordpress"></i> <a href="https://profiles.wordpress.org/<?php the_title_attribute(); ?>" title="Användarens profil på WordPress.org">Visa profil på WordPress.org</a></div>
 					<?php if ( get_post_meta($post->ID, '_wpsvse_translator_user_se', true) !='' ) { ?><div class="project-sv-repository"><i class="fa fa-user"></i> <a href="http://wpsv.se/medlemmar/<?php echo $translation_user; ?>/profile/" title="Användarens profil på WordPress Sverige">Visa profil på WordPress Sverige</a></div><?php } ?>
 				</div>
-        <div class="project-editors row">
+        <div class="project-editors">
 				<?php
 					// Find connected translators
 					$connected = new WP_Query( array(
@@ -55,11 +55,11 @@
 						 $project_icon = '<span class="fa-stack" title="WordPress"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-wordpress fa-stack-1x fa-inverse"></i></span> ';
 					 } 
 					 $project_id = get_post_meta($post->ID, '_wpsvse_project_project_id', true); ?>
-							<li class="col-md-4">
+							<li>
 							<div class="translation-user validator-project clearfix">
 								<?php echo $project_icon; ?>
 								<div class="profile-link wporg"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></div>
-								<div class="project-link-icons"><a href="https://translate.wordpress.org/locale/sv/default/<?php echo $project_type; ?>/<?php echo $project_id; ?>" title="Svenskt översättningsprojekt på WordPress.org"><i class="fa fa-language"></i></a><?php if ( !has_term( array ( 'meta','app','wordpress' ), 'wpsvse_project_type' ) ) { ?><a href="https://sv.wordpress.org/<?php echo $project_page; ?>/<?php echo $project_id; ?>/" title="Tema/tillägg i katalogen på WordPress.org"><i class="fa fa-wordpress"></i></a><?php } ?></div>
+								<div class="project-link-icons"><a href="https://translate.wordpress.org/locale/sv/default/<?php echo $project_type; ?>/<?php echo $project_id; ?>" title="Svenskt översättningsprojekt på WordPress.org"><i class="fa fa-language"></i> Visa översättningsprojekt</a><?php if ( !has_term( array ( 'meta','app','wordpress' ), 'wpsvse_project_type' ) ) { ?><a href="https://sv.wordpress.org/<?php echo $project_page; ?>/<?php echo $project_id; ?>/" title="Tema/tillägg i katalogen på WordPress.org"><i class="fa fa-wordpress"></i> Visa i katalogen på WordPress.org</a><?php } ?></div>
 							</div>
 							</li>
 					<?php endwhile; ?>
