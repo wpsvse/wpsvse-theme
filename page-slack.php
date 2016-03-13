35fe815b60701adae89ff569ecf04a8c1a1a3d1f
Mattias Tengblad <mst@eyesx.com>
2016-03-14 00:06:32 +0100

    Slack page template updates

--- page-slack.php
+++ page-slack.php
@@ -28,9 +28,10 @@ get_header();
 	<section id="page-full" class="section">
 		<div class="container">
 			<div class="row">
-        <div class="col-md-12 page-content">
+        <div class="col-md-9 page-content">
           <?php
-          the_content();
+					if ( function_exists( 'sharing_display' ) ) remove_filter( 'the_content', 'sharing_display', 19 );
+					the_content();
 
 					if ( is_user_logged_in() ) {
 					    if ( ! dynamic_sidebar( 'slack-members' ) ) : endif;
@@ -38,9 +39,12 @@ get_header();
 					    if ( ! dynamic_sidebar( 'slack' ) ) : endif;
 					}
 
+					if ( function_exists( 'sharing_display' ) ) echo sharing_display();
+
           edit_post_link( 'Redigera', '<span class="edit-link">', '</span>' );
           ?>
-        </div>
+				</div>
+        <?php get_sidebar(); ?>
 			</div>
 		</div>
 	</section>
